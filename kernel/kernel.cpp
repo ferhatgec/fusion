a8e98ded55c199dc2ad7be306b4c9b501efaf022
ferhatgec <ferhatgectao@gmail.com>
2020-10-28 22:37:33 +0300

    Unactive: MouseDriver interrupt.

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -132,10 +132,8 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     
     /* Keyboard & mouse handle interrupts & drivers (Generic PS/2) */
     KeyboardDriver keyboard(&interrupts);
-    MouseDriver mouse(&interrupts);
     
     RunShell();
-    //interrupts.Activate();
-
+    
     while(1);
 }
